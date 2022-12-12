f684630d9ba54f481c1b4f9bb049258e0b844f1c
Michelle Hendriks <106346778+mcphendriks@users.noreply.github.com>
2022-12-12 13:36:10 +0100

    UI event double click

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,3 +1,5 @@
+// Interaction Jump
+
 let interaction = document.querySelector('a:nth-of-type(12)')
 
 interaction.addEventListener('click', jumpHandler)
@@ -6,3 +8,18 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+// Interaction squat & stretch by dubble click
+
+let code = document.querySelector('a:nth-of-type(20)')
+
+// console.log(squatStretch)
+
+code.addEventListener('dblclick', squatHandler)
+
+function squatHandler() {
+  code.classList.toggle('squat-stretch')
+}
+
+
+
