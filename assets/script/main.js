d5ed6a70b41d4c0e2d6e6b354cad3d777f32497a
Michelle Hendriks <106346778+mcphendriks@users.noreply.github.com>
2023-01-30 20:52:34 +0100

    Add 4 ui-events

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,3 +1,36 @@
+
+// Interaction flip (10)
+
+let interface =document.querySelector('a:nth-of-type(10')
+
+console.log(interface)
+
+
+interface.onmousemove = function (e) {
+  let rect = e.target.getBoundingClientRect();
+
+  let x = e.clientX - rect.left;
+  interface.style.setProperty("--btn-11-x", x + "px");
+};
+  
+function mouseMove() {
+  interface.classList.toggle('mouse-cursor-tracking')
+}
+
+console.log(mouseMove)
+
+// Interaction flip (11)
+let events =document.querySelector('a:nth-of-type(11)')
+
+console.log(events)
+
+events.addEventListener('mousemove', mouseMove)
+
+function mouseMove () {
+  events.classList.toggle('flipping')
+}
+
+
 // Interaction Jump (12)
 
 let interaction = document.querySelector('a:nth-of-type(12)')
@@ -9,11 +42,44 @@ function jumpHandler() {
   interaction.classList.toggle('jump')
 }
 
+// Interaction (13) 
+  let userFlow = document.querySelector('a:nth-of-type(13)') 
+  console.log(userFlow)
+
+  if (userFlow) {
+    window.addEventListener('mousemove', (event) => {
+      const xPercent = event.clientX / window.innerWidth - 0.2
+      const yPercent = event.clientY / window.innerHeight - 0.5
+      userFlow.style.transform = `rotateX(${yPercent * 40}deg) rotateY(${xPercent * 40}deg)`
+	})
+  
+  window.addEventListener('mouseout', () => {
+		userFlow.style.transform = `rotateX(0) rotateY(0)`
+	})
+}
+
+// Interaction (14)
+
+let wireflow = document.querySelector('a:nth-of-type(14)')
+
+console.log(wireflow)
+
+wireflow.addEventListener('click', click)
+
+function click() {
+  wireflow.classList.toggle('mouse-gardient-animation')
+}
+
+
 // Interaction (15)
 
+let feedback = document.querySelector('a:nth-of-type(15)');
 
+window.addEventListener("scroll", scrollTrigger);
 
-
+function scrollTrigger() {
+  feedback.classList.toggle("spin-rotate");
+}
 
 
 //Interaction Key-up event & key-down (16)//
@@ -25,20 +91,19 @@ console.log(feedForward)
 feedForward.addEventListener('keydown', keydownhandler)
 feedForward.addEventListener('keyup', keyuphandler)
 
-function keyuphandler () {
-  feedForward.classList.toggle('key-up')
-}
-
 function keydownhandler() {
   feedForward.classList.toggle('key-down')
 }
 
+function keyuphandler() {
+  feedForward.classList.toggle('key-up')
+}
+
+
 // interaction rotate by keypress (17)
 let labels = document.querySelector('a:nth-of-type(17)')
 
-labels.addEventListener('keydown', dealWithKeyboard)
 labels.addEventListener('keypress', dealWithKeyboard)
-labels.addEventListener('keyup', dealWithKeyboard)
 
 console.log(labels)
 
@@ -63,17 +128,12 @@ function mouseIsMove () {
 
 let navigation = document.querySelector('a:nth-of-type(19)')
 
-navigation.addEventListener('mouseover', hovered)
-navigation.addEventListener('mouseout', hoveredOut)
-
 console.log(navigation)
 
-function hovered () {
-  navigation.classList.toggle('blow-up')
-}
+navigation.addEventListener('click', clickHandler)
 
-function hoveredOut() {
-  navigation.classList.toggle('blow-up')
+function clickHandler() {
+  navigation.classList.toggle('click-blow')
 }
 
 
